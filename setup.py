96a731287e884120927a49f4e4277c68c82093f5
Aleksandr Belov <alexbelov22@gmail.com>
2020-05-30 16:05:53 +0300

    reorganize project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='sr_kspace',
+   version='1.0',
+   description='Super-Resolution using low-frequency estimation of k-space data in MRI',
+   author=['Aleksandr Belov', 'Mariia Kopylova', 'Olga Shtepa', 'Margarita Sharkova', 'Tamara Tsakhilova'],
+   author_email=['Aleksandr.Belov@skoltech.ru', 'Mariia.Kopylova@skoltech.ru',
+                 'Olga.Shtepa@skoltech.ru', 'Margarita.Sharkova@skoltech.ru', 'Tamara.Tsakhilova@skoltech.ru'],
+   packages=['sr_kspace']
+)
